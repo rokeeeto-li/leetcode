0868fe76334df64832f7a3afe4db784989d049f2
Qihang Li <97816717+QH-Li-1999@users.noreply.github.com>
2022-04-19 17:08:38 -0400

    Create v1.py
    it looks pretty but have low time and space efficiency. Let me find a better way.

---
+++ Q0122_Stock_II/v1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(prices[i]-prices[i-1], 0)
+        return profit

3b14b270a4a88bc700a9d17efa09b803cfe983d2
Qihang Li <97816717+QH-Li-1999@users.noreply.github.com>
2022-05-27 23:57:05 -0400

    Create Iteration_method.py

---
+++ Q0021_Merge_Two_Sorted_Lists/Iteration_method.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(-1)
+        newList = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                newList.next = list1
+                list1 = list1.next
+            else:
+                newList.next = list2
+                list2 = list2.next
+            newList = newList.next
+        if list1:
+            newList.next = list1
+        else:
+            newList.next = list2
+        return head.next

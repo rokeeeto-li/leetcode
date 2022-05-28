72811f0f0f3fc98532e4e243c8b1ac92912c45a6
Qihang Li <97816717+QH-Li-1999@users.noreply.github.com>
2022-05-28 14:33:52 -0400

    Create Iterative.py

---
+++ Q0206_Reverse_Linked_List/Iterative.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr, next_temp = None, head, 0
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        return prev
